93129309bbeab9bebe3a7b76660910f8b3ac7d1c
fukiran <macdorphoto@yahoo.com>
2021-02-06 09:43:19 +0000

    add static config

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,9 +1,11 @@
-<template >
+<template lang="html">
 <div>
   <button class="button" v-on:click.once="setupLeafletMap() ">Where is Studio Ghibli Museum?</button>
-  <div id='mapContainer'>
-    <img src="../assets/tenor_down.gif" alt="tenor gif">
-  </div>
+
+<div id='mapContainer'>
+  <img src="../assets/tenor_down.gif" alt="tenor gif">
+</div>
+  
 </div>
 </template>
 
@@ -16,16 +18,26 @@ export default {
  
   data() {
    return{
-     center: [35.696190656717114, 139.57042410468455]
+     center: [35.696190656717114, 139.57042410468455],
+    //  icon: [{
+    //    iconUrl: '../assets/vector-mountains.jpeg',
+    //    iconSize: [38,95],
+    //    iconAnchor:   [22, 94],
+    //    popupAnchor:  [-3, -76]
+    //  }]
    }},
  methods: {
    setupLeafletMap: function () {
      
     const mapDiv = L.map("mapContainer").setView(this.center, 13);
-    const popup = L.popup()
-      .setLatLng([35.696190656717114, 139.57042410468455])
-      .setContent("Studio Ghibli Museum - Tokyo")
-      .openOn(mapDiv);
+    // const icon = L.marker()
+    //       .setLatLng([35.696190656717114, 139.57042410468455])
+    //       .addTo(mapDiv);
+    
+    const popup = L.marker([35.6, 139.5]).addTo(mapDiv);
+      // .setLatLng([35.696190656717114, 139.57042410468455])
+      // .setContent("Studio Ghibli Museum - Tokyo")
+      // .openOn(mapDiv);
     L.tileLayer(
        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
        {
@@ -34,7 +46,7 @@ export default {
          maxZoom: 18,
          
          id: "mapbox/satellite-v9",
-         accessToken: '{accessToken}'
+         accessToken: 'pk.eyJ1IjoicnVtYnVyYWswMDciLCJhIjoiY2trMDY3ZHdxMGVlMzJvcGMyZW8wMjU3dCJ9.iFV6KAaTxjBXXSh_WWRXCA'
        }
      ).addTo(mapDiv);
    },
